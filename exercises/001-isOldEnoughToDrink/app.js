adcd3c3e6080a4efbeaf0e325b00c4b7f4bed918
John Blanco <142819007+blancojohn@users.noreply.github.com>
2025-02-10 18:21:31 +0000

    Creación de función verificadora mayoría de edad

--- exercises/001-isOldEnoughToDrink/app.js
+++ exercises/001-isOldEnoughToDrink/app.js
@@ -1,4 +1,10 @@
 function isOldEnoughToDrink(age) {
+    let old= false
     // your code here
-    
+    if(age >= 21){
+        old= true
+    }
+    return old
 }
+
+console.log(isOldEnoughToDrink(21))
