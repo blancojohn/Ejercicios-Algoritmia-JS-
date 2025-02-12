0cbbbc3cc2eb36a2b0ce88e64edec51e2a95c98a
John Blanco <142819007+blancojohn@users.noreply.github.com>
2025-02-12 20:51:19 +0000

    Función que retorna el resultado de la suma de dos palabras

--- exercises/008-getLengthOfTwoWords/app.js
+++ exercises/008-getLengthOfTwoWords/app.js
@@ -1,4 +1,8 @@
 function getLengthOfTwoWords(word1, word2) {
   // your code here
-  
+  let result= word1.length + word2.length //guarda  la suma de letras de las dos palabras
+  return result
 }
+
+
+console.log(getLengthOfTwoWords("John Blanco", "Desarrollador de SoftWare"))
