db7cf4ddd55d475caac37c1e8b3faaa847dd53e3
John Blanco <142819007+blancojohn@users.noreply.github.com>
2025-02-12 20:41:04 +0000

    Función contadora de caracteres según un string pasado

--- exercises/007-getLengthOfWord/app.js
+++ exercises/007-getLengthOfWord/app.js
@@ -1,4 +1,7 @@
 function getLengthOfWord(word) {
   // your code here
-  
+  let numberLetters= word.length //Almacena la cantidad de letras
+  return numberLetters
 }
+
+console.log(getLengthOfWord("JOhnMarioBlanco"))
