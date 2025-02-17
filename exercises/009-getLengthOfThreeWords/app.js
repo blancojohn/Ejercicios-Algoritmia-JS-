368bcbb15542cf40631d2b10c2dbfbdf6e9dea2d
John Blanco <142819007+blancojohn@users.noreply.github.com>
2025-02-17 17:32:12 +0000

    Función contadora de letras de tres palabras

--- exercises/009-getLengthOfThreeWords/app.js
+++ exercises/009-getLengthOfThreeWords/app.js
@@ -1,6 +1,7 @@
 function getLengthOfThreeWords(word1, word2, word3) {
   // your code here
-  
+  let totalWords= word1.length + word2.length + word3.length
+  return totalWords
 }
 
 let output = getLengthOfThreeWords('some', 'other', 'words');
