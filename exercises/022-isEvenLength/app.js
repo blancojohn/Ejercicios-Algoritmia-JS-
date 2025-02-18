cb0115fabcb3561ea144eb7e8fe98861227edfc5
John Blanco <142819007+blancojohn@users.noreply.github.com>
2025-02-18 14:14:42 +0000

    Función que verifica si la cantidadde letras en una palabra es par o impar

--- exercises/022-isEvenLength/app.js
+++ exercises/022-isEvenLength/app.js
@@ -1,1 +1,8 @@
 // Write your function here
+const isEvenLength= (word)=>{
+    let quantityLetters= word.length
+    if(quantityLetters % 2 === 0) return true
+    else return false
+}
+
+console.log(isEvenLength("mario"))
