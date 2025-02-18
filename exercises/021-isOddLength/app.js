43f6068097d9946ed22c7d82d76fbb6a2b904bdd
John Blanco <142819007+blancojohn@users.noreply.github.com>
2025-02-18 14:07:15 +0000

    Función que verifica y retorna booleano si la cantiada de letras es par o impar

--- exercises/021-isOddLength/app.js
+++ exercises/021-isOddLength/app.js
@@ -1,1 +1,8 @@
 // Write your function here
+const isOddLength= (word)=>{
+    let numberLetters= word.length
+    if(numberLetters % 2 !== 0) return true
+    else return false 
+}
+
+console.log(isOddLength("Mary"))
