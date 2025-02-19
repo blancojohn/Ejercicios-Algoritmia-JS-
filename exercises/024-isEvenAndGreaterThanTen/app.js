45be638e67303cd67b597b3005dac0d67533797a
John Blanco <142819007+blancojohn@users.noreply.github.com>
2025-02-19 12:48:47 +0000

    Creación de función que verifica si un número es par y mayor a 10

--- exercises/024-isEvenAndGreaterThanTen/app.js
+++ exercises/024-isEvenAndGreaterThanTen/app.js
@@ -1,1 +1,8 @@
 // Write your function here
+const isEvenAndGreaterThanTen = (num)=> {
+    if(num > 10 && num % 2 === 0) return true
+    else return false
+}
+
+let outPut= isEvenAndGreaterThanTen(8)
+console.log(outPut)
