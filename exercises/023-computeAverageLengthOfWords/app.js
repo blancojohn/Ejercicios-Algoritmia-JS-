2f313bf899eb680f9f7f0796560f4028399166a9
John Blanco <142819007+blancojohn@users.noreply.github.com>
2025-02-19 12:41:47 +0000

    Creación de función que retorna el promedio de cantidad de letras entre dos palabras dadas

--- exercises/023-computeAverageLengthOfWords/app.js
+++ exercises/023-computeAverageLengthOfWords/app.js
@@ -1,1 +1,8 @@
 // Write your function here
+const computeAverageLengthOfWords= (word1, word2)=>{
+    let totalLetters= word1.length + word2.length
+    let promedio= totalLetters / 2
+    return promedio
+}
+
+console.log(computeAverageLengthOfWords("code", "programs"))
