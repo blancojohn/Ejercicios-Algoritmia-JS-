5dc9bc3e4cfdc89ce390713dd5c8b4dbed8255cf
John Blanco <142819007+blancojohn@users.noreply.github.com>
2025-02-19 12:53:39 +0000

    Función que calcula el promedio entre dos números dados

--- exercises/025-average/app.js
+++ exercises/025-average/app.js
@@ -1,1 +1,9 @@
 // Write your function here
+const average = (num1, num2)=> {
+    let total= num1 + num2 // suma de los dos valores pasados
+    let promedio= total / 2 // obtiene el promedio
+    return promedio
+}
+
+let outPut= average(4, 6)
+console.log(outPut)
