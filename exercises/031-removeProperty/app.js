dc3c304720634fd706e9b3c7f2f1d1a33f78d313
John Blanco <142819007+blancojohn@users.noreply.github.com>
2025-02-22 15:39:02 +0000

    Función que elimina la proiedad de un objeto al pasarle el objeto y la propiedad

--- exercises/031-removeProperty/app.js
+++ exercises/031-removeProperty/app.js
@@ -1,4 +1,16 @@
+let person=  {
+  name: 'John',
+  profession: 'Developer'
+}
+
 function removeProperty(obj, key) {
   // your code here
-  
+  return delete obj[key]
 }
+
+console.log(person)
+removeProperty(person, 'profession')
+console.log(person)
+console.log(person.profession)
+
+
