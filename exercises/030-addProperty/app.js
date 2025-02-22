afb2c694e891530c7ab04029d92538ffc5abd8f5
John Blanco <142819007+blancojohn@users.noreply.github.com>
2025-02-22 15:17:02 +0000

    Función para agregar una propiedad a un objeto y lo retorna

--- exercises/030-addProperty/app.js
+++ exercises/030-addProperty/app.js
@@ -1,4 +1,10 @@
+let myObj= {};
+
 function addProperty(obj, key) {
   // your code here
-  
-}
+  obj[key]= true
+  return obj
+};
+
+console.log(addProperty(myObj, "myProperty"));
+console.log(myObj.myProperty)
