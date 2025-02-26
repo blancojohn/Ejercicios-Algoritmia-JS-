d2e67658e8a4f6c6cd7e5ee0800f2253f8ce3d0e
John Blanco <142819007+blancojohn@users.noreply.github.com>
2025-02-26 20:57:22 +0000

    Función que agrega una tercera propiedad a un objeto dado con el valor de las otras propiedades

--- exercises/032-addFullNameProperty/app.js
+++ exercises/032-addFullNameProperty/app.js
@@ -1,4 +1,13 @@
+let person= {
+  firstName: 'John',
+  lastName: 'Mario'
+}
+
 function addFullNameProperty(obj) {
   // Add your code after this line
-  
+  objfullName= `${obj.firstName} ${obj.lastName}`
+  return obj
 }
+
+addFullNameProperty(person)
+console.log(person.fullName)
