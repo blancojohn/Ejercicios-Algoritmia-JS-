281453bc4a92fe25e99600ecfa147563ea9faae0
John Blanco <142819007+blancojohn@users.noreply.github.com>
2025-02-28 20:28:05 +0000

    Función que verifica el valor de una propiedad de un obejeto paasado

--- exercises/034-isPersonOldEnoughToDrive/app.js
+++ exercises/034-isPersonOldEnoughToDrive/app.js
@@ -1,4 +1,12 @@
+let obj= {
+  age: 16
+}
+
 function isPersonOldEnoughToDrive(person) {
   // Add your code after this line
-  
+  if(person.age >= 16) return true
+  else return false
 }
+
+let output= isPersonOldEnoughToDrive(obj)
+console.log(output)
