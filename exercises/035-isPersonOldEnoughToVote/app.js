6bfedefab6dc1ef6aea58c32f405f7bd15b2fd6e
John Blanco <142819007+blancojohn@users.noreply.github.com>
2025-02-28 20:33:50 +0000

    Función que verifica el valor de la propiedad de un objeto para poder cumplir con la edad para votar

--- exercises/035-isPersonOldEnoughToVote/app.js
+++ exercises/035-isPersonOldEnoughToVote/app.js
@@ -1,4 +1,12 @@
+let obj= {
+  age:16
+}
+
 function isPersonOldEnoughToVote(person) {
   // Add your code after this line
-  
+  if(person.age >= 18) return true
+  else return false
 }
+
+let output= isPersonOldEnoughToVote(obj);
+console.log(output)
