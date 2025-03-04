9216ff461cde7210e354b777f5d14430ec0ba46a
John Blanco <142819007+blancojohn@users.noreply.github.com>
2025-03-04 15:18:32 +0000

    Función que verifica el valor de la propiedad de un objeto pasado para saber mayoría de edad

--- exercises/036-isPersonOldEnoughToDrink/app.js
+++ exercises/036-isPersonOldEnoughToDrink/app.js
@@ -1,4 +1,14 @@
+let obj= {
+  age: 12
+}
+
 function isPersonOldEnoughToDrink(person) {
   // Add your code after this line
-  
+  /* Validavque cumple mayoría de edad y que el objeto pasado contiene
+    contiene la propiedad age */
+  if(person.age >= 21) return true 
+  else return false 
 }
+
+let output= isPersonOldEnoughToDrink(obj)
+console.log(output)
