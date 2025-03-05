6d872a0a6b15f2dcb6a0cf28a8dad85b5ed5768d
John Blanco <142819007+blancojohn@users.noreply.github.com>
2025-03-05 16:23:11 +0000

    Función crea un objeto con una propiedad y valor al recibir los parámetros

--- exercises/037-addArrayProperty/app.js
+++ exercises/037-addArrayProperty/app.js
@@ -1,4 +1,12 @@
+let myObj= {};
+let myArr= [1,2,3]
+
+
 function addArrayProperty(obj, key, arr) {
   // Add your code after this line
-  
+  obj[key]= arr
+  return obj
 }
+
+addArrayProperty(myObj, 'myProperty', myArr)
+console.log(myObj.myProperty)
