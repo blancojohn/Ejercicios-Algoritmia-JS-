aa9cb7ff175eb203488511b23441ad16c92f791e
John Blanco <142819007+blancojohn@users.noreply.github.com>
2025-08-14 01:39:10 +0000

    Función que calcula y triplica el área de un rectángulo

--- exercises/041-computeTripledAreaOfARectangle/app.js
+++ exercises/041-computeTripledAreaOfARectangle/app.js
@@ -1,6 +1,7 @@
 function computeTripledAreaOfARectangle(length, width) {
   // your code here
-  
+  let area= length * width
+  return area * 3
 }
 
 let output = computeTripledAreaOfARectangle(2, 4);
