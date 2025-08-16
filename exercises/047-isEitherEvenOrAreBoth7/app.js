d210e53a8b5e03c32859abaab49edd7a365491c8
John Blanco <142819007+blancojohn@users.noreply.github.com>
2025-08-16 18:31:24 +0000

    Función evalua si de dos numeros pasados uno es par o ambos son 7

--- exercises/047-isEitherEvenOrAreBoth7/app.js
+++ exercises/047-isEitherEvenOrAreBoth7/app.js
@@ -1,4 +1,12 @@
 function isEitherEvenOrAreBoth7(num1, num2) {
     // your code here
-    
+    let boolean;
+    if((num1 % 2 == 0 || num2 % 2 == 0) || (num1 == 7 && num2 == 7)){
+        boolean= true
+    }else {
+        boolean= false
+    }
+    return boolean
 }
+let outFunction= isEitherEvenOrAreBoth7(3,4)
+console.log(outFunction)
