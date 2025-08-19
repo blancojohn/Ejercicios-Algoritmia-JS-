910d9c28da574b453631dec955894375d5687c38
John Blanco <142819007+blancojohn@users.noreply.github.com>
2025-08-19 17:30:38 +0000

    Función evaluadora de condiciones

--- exercises/048-isEitherEvenAndLessThan9/app.js
+++ exercises/048-isEitherEvenAndLessThan9/app.js
@@ -1,7 +1,14 @@
 function isEitherEvenAndLessThan9(num1, num2) {
     // your code here
-
+    let boolean;
+    // evalua las condicines que uno de los parámetros sea un número par y los dos obligatoriamente menores que 9
+    if((num1 % 2 === 0 || num2 % 2 === 0) && (num1 < 9 && num2 < 9)){
+        boolean= true
+    }else{
+        boolean= false
+    }
+    return boolean
 }
 
-let output = isEitherEvenAndLessThan9(2, 4);
+let output = isEitherEvenAndLessThan9(6, 9);
 console.log(output); // --> true
