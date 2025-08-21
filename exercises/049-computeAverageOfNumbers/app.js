ae8d0955bf68ec81976bce1238abe43adb4bab8f
John Blanco <142819007+blancojohn@users.noreply.github.com>
2025-08-21 16:27:38 +0000

    Función que calcula el promedio con los valores de los item de un array dado

--- exercises/049-computeAverageOfNumbers/app.js
+++ exercises/049-computeAverageOfNumbers/app.js
@@ -1,1 +1,16 @@
 // Write your function here
+let input= [1,2,3,4,5]
+
+const computeAverageOfNumber= (array)=>{
+    let divisor= array.length //Valor que divide luego de sumar todos los valores
+    let resulatdoItemSuma= 0
+    let promedio;
+    for(let i= 0; i < array.length; i++){
+        resulatdoItemSuma += array[i] //Instrucción de asignar y sumar cada item el array por cada bucle  
+    }
+    promedio= resulatdoItemSuma / divisor 
+    return promedio
+}
+
+let output=  computeAverageOfNumber(input)
+console.log(output)
