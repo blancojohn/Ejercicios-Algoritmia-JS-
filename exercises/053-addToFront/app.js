f0331fc20cbad2feecb8af8464b593fb0d70982c
John Blanco <142819007+blancojohn@users.noreply.github.com>
2025-08-22 14:57:51 +0000

    función que agrega un elemento pasado al frente de un array pasado

--- exercises/053-addToFront/app.js
+++ exercises/053-addToFront/app.js
@@ -1,7 +1,8 @@
 function addToFront(arr, element) {
   // your code here
-  
+  arr.unshift(element) //agrega en la posición cero el elemento
+  return arr
 }
 
-let output = addToFront([1, 2], 3);
+let output = addToFront([1, 2], 6);
 console.log(output); // -> [3, 1, 2]
