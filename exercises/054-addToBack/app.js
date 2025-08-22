e7a0f016e12644ef0ceeb88ce8dd8015cb5fe5e8
John Blanco <142819007+blancojohn@users.noreply.github.com>
2025-08-22 15:01:42 +0000

    Función que añade al final de un array dado un item dado

--- exercises/054-addToBack/app.js
+++ exercises/054-addToBack/app.js
@@ -1,6 +1,7 @@
 function addToBack(arr, element) {
   // your code here
-  
+  arr.push(element) // Agrega el elemento pasado al final del array 
+  return arr
 }
 
 let output = addToBack([1, 2], 3);
