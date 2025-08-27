827d26c1dbdcf6451f274004af7fe5b40cc31e36
John Blanco <142819007+blancojohn@users.noreply.github.com>
2025-08-27 03:03:59 +0000

    Función que retorna un nuevo array a partir de la siguiente posición dada de un array dado

--- exercises/056-getElementsAfter/app.js
+++ exercises/056-getElementsAfter/app.js
@@ -1,6 +1,8 @@
 function getElementsAfter(array, n) {
   // your code here
-  
+  let indexSinguienteDelDado= n + 1
+  let nuevoArray= array.slice(indexSinguienteDelDado)
+  return nuevoArray
 }
 
 let output = getElementsAfter(['a', 'b', 'c', 'd', 'e'], 2); 
