75e80f7971ad73fc675f19e910fc96c680f0a1ba
John Blanco <142819007+blancojohn@users.noreply.github.com>
2025-09-06 19:16:26 +0000

    Función que cuenta cantidad de veces que aparece un caracter pasado en un string pasado

--- exercises/062-countCharacter/app.js
+++ exercises/062-countCharacter/app.js
@@ -1,6 +1,12 @@
-function countCharacter(str, char) {
+function countCharacter(str, char ) {
     // your code here
-    
+    let timesAppeared= 0;
+    for(let i= 0; i < str.length; i ++){
+        if( str[i] === char){
+            timesAppeared += 1
+        }
+    }
+    return timesAppeared
 }
 
 let output = countCharacter('I am a hacker', 'a');
