0e0d94e07bca910a112ababcad81b56f3cac16a8
John Blanco <142819007+blancojohn@users.noreply.github.com>
2025-09-16 01:23:39 +0000

    Función condicionada para recibir un string o un string vacío y retornar cada caracter dentro de un array o un array vacío

--- exercises/063-getAllLetters/app.js
+++ exercises/063-getAllLetters/app.js
@@ -1,6 +1,16 @@
 function getAllLetters(str) {
     // your code here
-    
+    let arrCharactersWord= [];
+
+    if(str === ''){
+        arrCharactersWord= []
+    }
+
+    for (let i= 0; i < str.length; i++){
+        let character= str[i]
+        arrCharactersWord.push(character)
+    }
+    return arrCharactersWord
 }
 
 let output = getAllLetters('Radagast');
